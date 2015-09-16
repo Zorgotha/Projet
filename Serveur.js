ef6ac2170686ac34ea000db52cef3d25d3866529
Zorgotha <benoit.kopp95@orange.fr>
2015-09-16 07:50:48 -0700

    Serveur HTTPS

--- Serveur.js
+++ Serveur.js
@@ -1,14 +1,20 @@
-var http = require('http'),
+var https = require('https'),
     fs = require('fs');
+
+var options = {
+	key: fs.readFileSync('./PrivateKey.pem', 'utf8'),
+	cert: fs.readFileSync('./cert.pem', 'utf8')
+};
+
 fs.readFile('./index.html', function (err, html){
 	if(err){
 		throw err;
 	}
-var server = http.createServer(function(req,res){
+var server = https.createServer(options, function(req,res){
     res.writeHead(200,{"Content-Type": "text/html"});
     res.write(html);
     res.end();
-   }).listen(8080);
+   }).listen(8000);
 });
 //server.listen(8080);
 //console.log("Server is listening");
