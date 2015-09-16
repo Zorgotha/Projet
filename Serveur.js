5e07a98b8d63766eb6f63056bbda227b099ee1d4
Zorgotha <benoit.kopp95@orange.fr>
2015-09-16 06:26:00 -0700

    Serveur HTTP

--- Serveur.js
+++ Serveur.js
@@ -1,8 +1,14 @@
-var http = require('http');
+var http = require('http'),
+    fs = require('fs');
+fs.readFile('./index.html', function (err, html){
+	if(err){
+		throw err;
+	}
 var server = http.createServer(function(req,res){
-    res.writeHead(200);
-    res.end("hello world\n");
+    res.writeHead(200,{"Content-Type": "text/html"});
+    res.write(html);
+    res.end();
+   }).listen(8080);
 });
-
-server.listen(8080);
-console.log("Server is listening");
+//server.listen(8080);
+//console.log("Server is listening");
