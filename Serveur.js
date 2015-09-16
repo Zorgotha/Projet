162e9e51c55aa13bf6aeeb22193dcde5542a6fa0
Zorgotha <benoit.kopp95@orange.fr>
2015-09-16 06:03:29 -0700

    Serv HTTP

--- Serveur.js
+++ Serveur.js
@@ -1,12 +1,8 @@
-var https = require('https');
-var fs = require('fs');
-
-var options = {
-    key: fs.readFileSync('test/fixtures/keys/agent2-key.pem');
-    cert:fs.readFileSync('test/fixtures/keys/agent2-cert.pem');
-};
-
-https.createServer(options, function(req,res){
+var http = require('http');
+var server = http.createServer(function(req,res){
     res.writeHead(200);
     res.end("hello world\n");
-}).listen(8000);
+});
+
+server.listen(8080);
+console.log("Server is listening");
