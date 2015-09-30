052b1476697f11965c02a1f7fd19f8f85257d430
Zorgotha <benoit.kopp95@orange.fr>
2015-09-30 07:16:18 -0700

    Nouvelle version serveur https avec express

---
+++ server.js
@@ -0,0 +1,16 @@
+var fs = require('fs');
+var https = require('https');
+var privateKey  = fs.readFileSync('./key.pem');
+var certificate = fs.readFileSync('./cert.pem');
+
+var credentials = {key: privateKey, cert: certificate};
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname));
+
+var httpsServer = https.createServer(credentials, app);
+console.log('HTTPS Server listening');
+
+// spin up server
+httpsServer.listen(8700);
